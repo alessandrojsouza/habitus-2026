292b4c7ba6ede1fb6f8d283f4dcea6e60dadd6ab
Tales Lourenço <161078229+taleslourenco@users.noreply.github.com>
2026-05-15 14:44:04 +0000

    alterações para pasta media

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+# Garante a existência e permissões da pasta media no boot
+mkdir -p /usr/src/python/app/devadmin/media
+chmod -R 777 /usr/src/python/app/devadmin/media
+
 # Aplica as migrações do banco de dados
 echo "Aplicando migrações..."
 python manage.py migrate --noinput
